3ddf92b1bcfb7f8df6d2b436609fdedb0243da8a
haneetsinghchhabra <HOTh@n!!t52p93>
2021-08-09 19:48:14 +0530

    isValid

--- app/src/main/java/com/technorapper/datastructure/ds/DiffrentQuestions.java
+++ app/src/main/java/com/technorapper/datastructure/ds/DiffrentQuestions.java
@@ -1,126 +1,117 @@
 package com.technorapper.datastructure.ds;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class DiffrentQuestions {
 
     public static void main(String[] args) {
 
 
-        // Given string of number
-        String s = "123";
-
-        // Function Call
-       // convert(s);
-
-
-        int [][]a = { { 1, 2, 3, 4 },
-                { 5, 6, 7, 8 },
-                { 1, 2, 3, 4 },
-                { 5, 6, 7, 8 } };
-
-        System.out.println(isNumber( "Infinity"));
+        int[] arr = {2, 5, 10, 12, 15, 19, 22, 24};
+        System.out.println(getArray(arr, 25));
     }
 
-    public static void convert(String s)
-    {
+    public static void convert(String s) {
 
         // Initialize a variable
         int num = 0;
         int n = s.length();
 
         // Iterate till length of the string
-        for(int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++)
 
             // Subtract 48 from the current digit
-            num = num * 10 + ((int)s.charAt(i) - 48);
+            num = num * 10 + ((int) s.charAt(i) - 48);
 
         // Print the answer
         System.out.print(num);
     }
 
 
-
-
-    static void printDiagonalSums(int [][]mat,
-                                  int n)
-    {
+    static void printDiagonalSums(int[][] mat,
+                                  int n) {
         int principal = 0, secondary = 0;
         for (int i = 0; i < n; i++) {
             principal += mat[i][i];
             secondary += mat[i][n - i - 1];
         }
 
-      int[] arr= {4,5,6,7,0,1,2};
-        System.out.println(search(arr,6));
+        int[] arr = {4, 5, 6, 7, 0, 1, 2};
+        System.out.println(search(arr, 6));
     }
 
 
-    public static  int search(int[] nums, int target) {
+    public static int search(int[] nums, int target) {
 
         //first find the index of the minimum element
         int index = index(nums);
 
         //if no rotation is done, we can simply call binary serch on whole range of array
-        if(index == 0){
-            return binarySearch(nums, target, 0, nums.length-1);
+        if (index == 0) {
+            return binarySearch(nums, target, 0, nums.length - 1);
         }
 
         //if target is less than nums[0], that means we have to search in the increasing array after pivot element
-        if(target < nums[0]){
-            return binarySearch(nums, target, index, nums.length-1);
-        }
-        else{
+        if (target < nums[0]) {
+            return binarySearch(nums, target, index, nums.length - 1);
+        } else {
             //search in increasing array before the pivot element
             return binarySearch(nums, target, 0, index);
         }
     }
 
-    public static int binarySearch(int[] nums, int target, int left, int right){
+    public static int binarySearch(int[] nums, int target, int left, int right) {
 
-        while(left <= right){
-            int mid = left + (right - left)/2;
-            if(nums[mid] == target){
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
                 return mid;
             }
-            if(nums[mid] < target){
-                left = mid+1;
-            }
-            else{
-                right=  mid-1;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
 
         return -1;
     }
-    public static int index(int[] nums){
+
+    public static int index(int[] nums) {
 
 
-        if(nums.length == 1 || nums[0] < nums[nums.length-1]){
+        if (nums.length == 1 || nums[0] < nums[nums.length - 1]) {
             return 0;
         }
 
         int left = 0;
-        int right = nums.length-1;
+        int right = nums.length - 1;
 
-        while(left <= right){
+        while (left <= right) {
 
-            int mid = left + (right - left)/2;
+            int mid = left + (right - left) / 2;
 
-            if(nums[mid] > nums[mid+1]){
-                return mid+1;
+            if (nums[mid] > nums[mid + 1]) {
+                return mid + 1;
             }
-            if(nums[mid] < nums[mid-1]){
+            if (nums[mid] < nums[mid - 1]) {
                 return mid;
             }
 
-            if(nums[mid] > nums[0]){
-                left = mid+1;
-            }
-            else{
-                right = mid-1;
+            if (nums[mid] > nums[0]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
         return -1;
     }
+
     public static boolean isNumber(String s) {
 
         int size = s.length(), eCount = 0, ECount = 0, signCount = 0, dotCount = 0, indexE = -1, indexSign1 = -1, indexSign2 = -1, indexDot = -1;
@@ -217,4 +208,74 @@ public class DiffrentQuestions {
         return true;
     }
 
+
+    public static void checkThis() {
+
+        {
+            int[] arr = {1, 3, 4, 6, 8, 10};
+            Map<Integer, Integer> map = new HashMap<>();
+            int length = arr[arr.length - 1] - arr.length;
+            int count = 0;
+            ArrayList<Integer> newArr = new ArrayList<>();
+            int initial = arr[0];
+            for (int i : arr) {
+                map.put(i, 1);
+            }
+
+            while (initial != arr[arr.length - 1]) {
+                if (!map.containsKey(initial)) {
+                    newArr.add(initial);
+                }
+                initial++;
+                count++;
+
+            }
+
+
+            for (int i : newArr) {
+                System.out.println(i + ", ");
+            }
+
+        }
+    }
+
+    // Returns minimum possible pairwise
+    // absolute difference of two arrays.
+    static long findMinSum(long a[], long b[], long n) {
+        // Sort both arrays
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        // Find sum of absolute differences
+        long sum = 0;
+        for (int i = 0; i < n; i++)
+            sum = sum + Math.abs(a[i] - b[i]);
+
+        return sum;
+    }
+
+
+    static int[] getArray(int[] a, int sum) {
+        int[] newArr = new int[2];
+        Set<Integer> valSet = new HashSet<>();
+        int temp;
+        for (int i : a) {
+            valSet.add(i);
+        }
+
+        for (int x : a) {
+            temp = sum - x;
+            if (valSet.contains(temp)) {
+                newArr[0] = x;
+                newArr[1] = temp;
+                break;
+            }
+        }
+
+        return newArr;
+    }
+
+
 }
+
+
