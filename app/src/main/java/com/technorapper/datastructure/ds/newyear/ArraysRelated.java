652b45f6c7e603bea08c57855db5a56fdc194d2e
Haneet Singh Chhabra <haneet555@gmail.com>
2022-01-14 00:30:07 +0530

    https://leetcode.com/problems/maximum-subarray/

--- app/src/main/java/com/technorapper/datastructure/ds/newyear/ArraysRelated.java
+++ app/src/main/java/com/technorapper/datastructure/ds/newyear/ArraysRelated.java
@@ -1,4 +1,26 @@
 package com.technorapper.datastructure.ds.newyear;
 
 public class ArraysRelated {
+
+    public static int maxSubArray(int[] nums) {
+        int n = nums.length;
+        int max = Integer.MIN_VALUE, sum = 0;
+
+        for(int i=0;i<n;i++){
+            sum += nums[i];
+            max = Math.max(sum,max);
+
+            if(sum<0) sum = 0;
+        }
+
+        return max;
+    }
+
+    // Driver code
+    public static void main(String[] args) {
+
+
+        System.out.println(maxSubArray(new int[]{-2,1,-3,4,-1,2,1,-5,4}));
+
+    }
 }
