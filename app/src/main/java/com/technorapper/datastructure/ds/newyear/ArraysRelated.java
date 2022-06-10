783c3fd3ba5d33d4832b47c98db22175e6dd2b02
Haneet Singh Chhabra <haneet555@gmail.com>
2022-06-11 01:06:31 +0530

    workibng

--- app/src/main/java/com/technorapper/datastructure/ds/newyear/ArraysRelated.java
+++ app/src/main/java/com/technorapper/datastructure/ds/newyear/ArraysRelated.java
@@ -156,7 +156,44 @@ public class ArraysRelated {
         }
         return reverse == x;
     }
+    public String longestPalindrome(String s) {
+        int start = 0;
+        int end = 0;
+        int maximum = 0;
 
+        for (int i = 0; i < s.length(); i++) {
+            int left = i; // start with one letter
+            int right = i;
+
+            while (left - 1 >= 0 && right + 1 < s.length() &&
+                    s.charAt(left - 1) == s.charAt(right + 1)) {
+                left--;
+                right++;
+            }
+
+            if (left >= 0 && right < s.length() && right - left + 1 > maximum) {
+                maximum = right - left + 1;
+                start = left;
+                end = right + 1;
+            }
+
+            left = i + 1; // start with two letter
+            right = i;
+            while (left - 1 >= 0 && right + 1 < s.length() &&
+                    s.charAt(left - 1) == s.charAt(right + 1)) {
+                left--;
+                right++;
+            }
+
+            if (left >= 0 && right < s.length() && right - left + 1 > maximum) {
+                maximum = right - left + 1;
+                start = left;
+                end = right + 1;
+            }
+        }
+
+        return s.substring(start, end);
+    }
     private static Set createSetFromHere(int i, int[] nums) {
         Set set = new HashSet();
         for (int x = 0; x < nums.length; x++) {
