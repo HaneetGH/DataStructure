2a8b7e7e68d0d809efb6ed60807578c337032a33
Haneet Singh Chhabra <haneet555@gmail.com>
2022-06-16 01:28:25 +0530

    workibng

--- app/src/main/java/com/technorapper/datastructure/ds/newyear/ArraysRelated.java
+++ app/src/main/java/com/technorapper/datastructure/ds/newyear/ArraysRelated.java
@@ -201,10 +201,40 @@ public class ArraysRelated {
             if (nums[x] != i)
                 set.add(nums[x]);
         }
-
         return set;
     }
+    public int solution1(int[] A) {
+        if (A.length == 0 || A.length== 1) {
+            return A.length;
+        }
+        int startingIndex = 0;
+        int endingIndex = 0;
+        int[] locationVisitedCounter = new int[A.length];
+        locationVisitedCounter[A[0] - 1] = 1;
 
+        for (var i=1; i<A.length; i++)
+        {
+            int locationIndex = A[i] - 1;
+
+            locationVisitedCounter[locationIndex]++;
+
+            if (A[i] == A[i - 1])
+            {
+                continue;
+            }
+
+            endingIndex=i;
+
+            while (locationVisitedCounter[A[startingIndex] - 1] > 1)
+            {
+                locationVisitedCounter[A[startingIndex] - 1]--;
+                startingIndex++;
+            }
+
+        }
+
+        return endingIndex - startingIndex + 1;
+    }
     public int trap(int[] height) {
 
         int maxIndex = -1;
